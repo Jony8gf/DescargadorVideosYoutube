08af7cc8836a121a4d0aa6a31d6fc12813b67f89
Jonathan <jgonzalezf09@educantabria.es>
2021-02-03 17:31:45 +0100

    Version Final

---
+++ main.py
@@ -0,0 +1,23 @@
+# Este proyecto permite las descarga de videos de Youtube
+
+# Para instalar PYTUBE, instala esta línea en tu consola/Terminal:
+#  pip install pytube
+from pytube import YouTube
+
+#Le indicamos por teclado la url del video que deseamos
+
+yt=YouTube('https://www.youtube.com/watch?v=WQ7gYKh5mj4&t=3s&ab_channel=JonyCoding')
+
+#Atributos iteresantes
+#Title nos indica el nombre del video
+print(yt.title)
+#Autor nos indica el canal del Youtube de donde procede dicho video
+print(yt.author)
+#Length nos indica el tiempo total en segundos
+print(yt.length)
+
+#Descargar el video, y con la funcion de Highest_Resolution
+#Para que este en buena calidad videografica
+yt.streams.get_highest_resolution().download()
+
+
